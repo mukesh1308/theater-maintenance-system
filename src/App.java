108bf379f4d92d711531727a4b9446300318934d
m mukesh kumar <mukeshkumar130803@gmail.com>
2024-02-18 09:29:18 +0530

    database connection

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.sql.SQLException;
+
+import Models.Connect;
+import Util.Input;
+
+public class App extends Input {
+    public static void main(String[] args) {
+        try{
+            Connect.getConnection();
+        }
+        catch(SQLException err){
+            System.out.println(err.getMessage());
+            return;
+        }
+        
     }
 }
---
+++ src/Models/Connect.java
@@ -0,0 +1,20 @@
+package Models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import Util.Env;
+
+public class Connect {
+    protected static Connection conn;
+    protected static Statement statement;
+    private Connect(){}
+
+    public static void getConnection() throws SQLException{
+        conn=DriverManager.getConnection(Env.databaseURL, Env.databaseUser, Env.databasePassword);
+        statement=conn.createStatement();
+    }
+
+}
