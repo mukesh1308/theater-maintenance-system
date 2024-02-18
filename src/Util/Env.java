c8af42a3f6fd724124d374339a8a88ba4ffa0ef6
m mukesh kumar <mukeshkumar130803@gmail.com>
2024-02-18 09:06:34 +0530

    first commit

---
+++ src/Util/Env.java
@@ -0,0 +1,7 @@
+package Util;
+
+public class Env {
+    public static String databaseURL="jdbc:mysql://localhost/theater";
+    public static String databaseUser="root";
+    public static String databasePassword="root";
+}
